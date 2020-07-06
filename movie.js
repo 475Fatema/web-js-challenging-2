1e920959eb99648860e8e63bd85563aedbd9520e
475Fatema <66346391+475Fatema@users.noreply.github.com>
2020-07-06 22:52:54 +0300

    سوف اكمل عليه في الغد

---
+++ movie.js
@@ -0,0 +1,12 @@
+let movies_list = [" Harry potter","sonic"," avengers endgame","IT Chapter2"];
+
+function movie(add) {
+    for (const movies of movies_list) {
+        console.log()
+        
+    }
+}
+
+
+
+let fav =[];
